8024eafb2d9840c53a8a57c133e4433c3cb9b01e
Luan Tedesco <luanfelipe2040@gmail.com>
2023-06-27 10:05:58 -0300

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'guests/family_invite', to: 'guests#family_invite'
+  get 'guests/guest_invite', to: 'guests#guest_invite'
+  get 'guests/sponsor_invite', to: 'guests#sponsor_invite'
+  get 'guests/new_family_presence', to: 'guests#new_family_presence'
+  get 'guests/new_guest_presence', to: 'guests#new_guest_presence'
+  get 'guests/new_sponsor_presence', to: 'guests#new_sponsor_presence'
+  post 'guests/create_family_presence', to: 'guests#create_family_presence', as: 'create_family_presence'
+  post 'guests/create_guest_presence', to: 'guests#create_guest_presence', as: 'create_guest_presence'
+  post 'guests/create_sponsor_presence', to: 'guests#create_sponsor_presence', as: 'create_sponsor_presence'
 end
